86d4952d655e331f021c3046a63364aecea0b732
ImanliOmer <100156424+ImanliOmer@users.noreply.github.com>
2023-01-26 18:22:12 +0400

    All of them are uploading

---
+++ InheritanceP331/InheritanceP331/ExpertDocumentProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceP331
+{
+    internal class ExpertDocumentProgram:ProDocumentProgram
+    {
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Document openend Saved in pdf format");
+        }
+    }
+}
---
+++ InheritanceP331/InheritanceP331/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace InheritanceP331
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Tryagain: Console.WriteLine("Enter user access level");
+            var user = Console.ReadLine();
+
+            if (user == "basic")
+            {
+                var basic = new DocumentProgram();
+                basic.level = "Basic";
+                basic.OpenDocument();
+                basic.EditDocument();
+                basic.SaveDocument();
+
+            }
+            else if (user == "pro")
+            {
+                var pro = new ProDocumentProgram();
+                pro.level = "Pro";
+                pro.OpenDocument();
+                pro.EditDocument();
+                pro.SaveDocument();
+            }
+            else if (user == "expert")
+            {
+                var expert = new ExpertDocumentProgram();
+                expert.level = "Expert";
+                expert.OpenDocument();
+                expert.EditDocument();
+                expert.SaveDocument();
+            }
+            else
+            {
+                Console.WriteLine("Access level doesnt true");
+                goto Tryagain;
+            }
+        }
+    }
+}
